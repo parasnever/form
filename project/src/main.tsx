f06c014ebb05e6bda39c98d2758fce85e55ad670
BecomingParas <123073613+parasnever@users.noreply.github.com>
2024-02-04 06:20:41 +0000

    this is

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App.tsx";
+import "./index.css";
+import { Score } from "./components/counter";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <h2>Quiz Game</h2>
+    <Score teamName="A" counterName="Score" />
+    <Score teamName="B" counterName="Score" />
+
+    <h2>Football</h2>
+    <Score teamName="Liverpool" counterName="Goals" />
+    <Score teamName="Chelsea" counterName="Goals" />
+  </React.StrictMode>
+);
